01c77e2242354ca5362ee6f13c8e98357653a51c
Joe Stephens <jlcstephens@gmail.com>
2024-10-27 16:39:26 +0000

    Migrate to next 15

--- app/[[...page]]/page.tsx
+++ app/[[...page]]/page.tsx
@@ -1,21 +1,20 @@
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../components/builder";
 import Footer from "@/components/footer/Footer";
-import { InferGetStaticPropsType } from "next";
-import { getStaticProps } from "next/dist/build/templates/pages";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-interface PageProps extends InferGetStaticPropsType<typeof getStaticProps> {}
+type Params = Promise<{ page: string[] }>
 
-export default async function Page(props: PageProps) {
+export default async function Page(props: { params: Params }) {
   await import('isolated-vm');
   const builderModelName = "page";
+  const params = await props.params;
 
   const content = await builder
     .get(builderModelName, {
       userAttributes: {
-        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+        urlPath: "/" + (params?.page?.join("/") || ""),
       },
       cacheSeconds: 20,
       staleCacheSeconds: 0
